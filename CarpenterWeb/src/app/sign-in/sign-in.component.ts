aaeed1b4890000aae478a4fb69a799e0684ff467
Josiah Wood <jnwood@gmail.com>
2023-09-11 21:34:37 -0500

    working on auth

--- CarpenterWeb/src/app/sign-in/sign-in.component.ts
+++ CarpenterWeb/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserInfo } from '../models/user-info';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+  userInfo!: UserInfo;
 
+  async ngOnInit() {
+    this.userInfo = await this.getUserInfo();
+  }
+
+  async getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
+  }
 }
