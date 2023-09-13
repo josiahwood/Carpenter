06692041885a5f4b321c6d913808f3271796e32e
Josiah Wood <jnwood@gmail.com>
2023-09-13 17:22:29 -0500

    only returning not done generations

--- CarpenterApi/Models/MessageGeneration.cs
+++ CarpenterApi/Models/MessageGeneration.cs
@@ -143,7 +143,7 @@ namespace CarpenterApi.Models
         public static async Task<IEnumerable<MessageGeneration>> GetNotDoneMessageGenerations(CosmosClient client, CarpenterUser user)
         {
             Container container = client.GetDatabase("carpenter-dev").GetContainer("message-generations");
-            QueryDefinition queryDefinition = new QueryDefinition("SELECT * FROM c WHERE c.userId = @searchterm").WithParameter("@searchterm", user.userId);
+            QueryDefinition queryDefinition = new QueryDefinition("SELECT * FROM c WHERE c.userId = @searchterm AND c.status != 'done'").WithParameter("@searchterm", user.userId);
 
             List<MessageGeneration> messageGenerations = new();
 
