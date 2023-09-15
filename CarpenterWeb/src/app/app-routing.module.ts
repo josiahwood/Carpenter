b012e0d262960b4d6fb80b715b2db2537a597548
Josiah Wood <jnwood@gmail.com>
2023-09-15 12:49:58 -0500

    fixing routing

--- CarpenterWeb/src/app/app-routing.module.ts
+++ CarpenterWeb/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatLogComponent } from './chat-log/chat-log.component';
 import { ChatMemoryComponent } from './chat-memory/chat-memory.component';
+import { MessageGenerationsComponent } from './message-generations/message-generations.component';
 
 const routes: Routes = [
   { path: 'chat-log', component: ChatLogComponent },
-  { path: 'chat-memory', component: ChatMemoryComponent }
+  { path: 'chat-memory', component: ChatMemoryComponent },
+  { path: 'message-generations', component: MessageGenerationsComponent }
 ];
 
 @NgModule({
